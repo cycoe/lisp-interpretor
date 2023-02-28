922ea6d16d94e0a0fea767599498300d365ce455
cycoe <cycoejoo@163.com>
2023-02-28 17:29:29 +0800

    [ADDED] Add lispP

--- Libs/Parser.hs
+++ Libs/Parser.hs
@@ -1,7 +1,8 @@
+
 module Libs.Parser where
 
 import Text.ParserCombinators.Parsec (GenParser, option, string, many1, many, digit,
-                                      oneOf, letter, (<|>))
+                                      oneOf, letter, (<|>), try)
 import Libs.Expr (LispExpr(..))
 
 intP :: GenParser Char st LispExpr
@@ -16,3 +17,6 @@ symbolP = LispSymbol <$> do
   r <- many $ firstAllowed <|> digit
   return (f:r) where
     firstAllowed = oneOf "+-*/" <|> letter
+
+lispP :: GenParser Char st LispExpr
+lispP = try intP <|> try symbolP
