77944ab7000cfae8b92c88a42a0cf34dcfe2932a
cycoe <cycoejoo@163.com>
2023-02-28 21:21:52 +0800

    [ADDED] Add eval function for LispInt and LispSymbol

--- Libs/Expr.hs
+++ Libs/Expr.hs
@@ -1,9 +1,17 @@
 module Libs.Expr where
 
+import qualified Data.Map as Map
+
 data LispExpr = LispInt Integer
               | LispSymbol String
+
+type Context = Map.Map String LispExpr
 
 instance Show LispExpr where
   show :: LispExpr -> String
   show (LispInt i)    = show i
   show (LispSymbol s) = s
+
+eval :: Context -> LispExpr -> LispExpr
+eval ctx (LispInt i)    = (LispInt i)
+eval ctx (LispSymbol s) = ctx Map.! s
